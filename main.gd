3fe9bf673aa7d5a5a1e83af5b89d3c9ac870a6f3
Ikkebi <noah@merrild.cc>
2023-10-09 15:11:34 +0200

    enemy and movement

--- main.gd
+++ main.gd
@@ -5,7 +5,8 @@ export (int) var gravity = 2000
 onready var visibility =$VisibilityNotifier2D
 
 var velocity := Vector2.ZERO
-var x_direction = -1
+var x_direction = 1
+
 
 # Called when the node enters the scene tree for the first time.
 func _physics_process(delta):
@@ -15,8 +16,3 @@ func _physics_process(delta):
 		velocity = move_and_slide(velocity, Vector2.UP)
 		if is_on_wall():
 			x_direction = -x_direction
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
